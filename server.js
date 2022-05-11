9287fe90a8f92737e4ff5930ef68762d229b091c
piruthuvi22 <piruthuviraj22@gmail.com>
2022-05-11 14:34:43 +0530

    heroku push

--- server.js
+++ server.js
@@ -24,7 +24,8 @@ app.use("/customers", customers_routes);
 app.use("/suppliers", suppliers_routes);
 app.use("/products", products_routes);
 
-app.get("/", (req, res) => console.log("Server running at port ", PORT));
+app.get("/", (req, res) => res.send("Server running at port ", PORT));
+
 mongoose.connect(MONGO_URI, (err) => {
   if (err) console.log(err);
   console.log("DB connection established");
