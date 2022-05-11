73803cf7a78125ac3e8440d81707bac4afab5be4
piruthuvi22 <piruthuviraj22@gmail.com>
2022-05-11 11:09:32 +0530

    second

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ const products_routes = require("./routes/products_routes");
 
 const app = express();
 
-const MONGO_URI = "mongodb://localhost:27017/Accounts";
-// const MONGO_URI = process.env.MONGO_URI;
+// const MONGO_URI = "mongodb://localhost:27017/Accounts";
+const MONGO_URI = process.env.MONGO_URI;
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
